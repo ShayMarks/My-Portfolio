e4d83a070dad7f4f34b1a4b20fd2f5a47c24e497
shay <nrexhd@gmail.com>
2023-09-26 04:30:15 +0300

    First update
    Updating the project files

---
+++ script.js
@@ -0,0 +1,33 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+// Get references to the modal, button, and close button
+const modal = document.getElementById('resumeModal');
+const openButton = document.getElementById('openResumeButton');
+const closeButton = document.getElementById('closeResumeButton');
+
+openButton.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+// Close the modal if the user clicks outside the modal content
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
+
