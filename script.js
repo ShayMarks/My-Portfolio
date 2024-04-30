d122e2c772858fc0bba1af16355c9d0abeb1da61
shay <nrexhd@gmail.com>
2024-04-30 19:12:20 +0300

    update new projects
    :dolphin:

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
 // Smooth scrolling for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
-
 
 // Get references to the modal, button, and close button
-const modal = document.getElementById('resumeModal');
-const openButton = document.getElementById('openResumeButton');
-const closeButton = document.getElementById('closeResumeButton');
+const modal = document.getElementById("resumeModal");
+const openButton = document.getElementById("openResumeButton");
+const closeButton = document.getElementById("closeResumeButton");
 
-openButton.addEventListener('click', () => {
-    modal.style.display = 'block';
+openButton.addEventListener("click", () => {
+  modal.style.display = "block";
 });
 
-closeButton.addEventListener('click', () => {
-    modal.style.display = 'none';
+closeButton.addEventListener("click", () => {
+  modal.style.display = "none";
 });
 
 // Close the modal if the user clicks outside the modal content
-window.addEventListener('click', (event) => {
-    if (event.target === modal) {
-        modal.style.display = 'none';
-    }
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
 });
-
-
